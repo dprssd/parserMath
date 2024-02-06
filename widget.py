2a2be3d6af1491199ca9281846e86d09c4cfd837
Aleksey Tsybikov <tsibikow@mail.ru>
2024-02-06 16:20:14 +0800

    upd 30.01 17:34 main

--- widget.py
+++ widget.py
@@ -1,30 +1,55 @@
-import flet as ft
+import sys
+from PyQt5 import QtCore, QtGui, QtWidgets
+from bson.objectid import ObjectId
+from pymongo import MongoClient
+import mainWindow
 
 
-def main(page: ft.Page):
-    page.title = "Flet counter example"
-    page.vertical_alignment = ft.MainAxisAlignment.CENTER
+class mainWin(QtWidgets.QMainWindow, mainWindow.Ui_MainWindow):
+    def __init__(self):
+        super().__init__()
+        self.client = MongoClient('localhost', 27017)
+        self.db = self.client['test']
+        self.collection = self.db['math']
+        self.setupUi(self)
+        self.tableWidget.setHorizontalHeaderLabels(['Key', 'Value', 'Type'])
+        self.tableWidget.resizeColumnsToContents()
+        self.name_comboBox.currentTextChanged.connect(self.click_combobox)
+        self.load_combobox()
 
-    txt_number = ft.TextField(value="0", text_align=ft.TextAlign.RIGHT, width=100)
+    def load_combobox(self):
+        all_documents = self.collection.find()
+        for document in all_documents:
+            self.name_comboBox.addItem(str(document['_id']))
 
-    def minus_click(e):
-        txt_number.value = str(int(txt_number.value) - 1)
-        page.update()
+    def enter_key_value(self, id_document):
 
-    def plus_click(e):
-        txt_number.value = str(int(txt_number.value) + 1)
-        page.update()
+        self.tableWidget.clearContents()
+        self.tableWidget.setRowCount(0)
 
-    page.add(
-        ft.Row(
-            [
-                ft.IconButton(ft.icons.REMOVE, on_click=minus_click),
-                txt_number,
-                ft.IconButton(ft.icons.ADD, on_click=plus_click),
-            ],
-            alignment=ft.MainAxisAlignment.CENTER,
-        )
-    )
+        record = self.collection.find_one({"_id": id_document})
 
-ft.app(target=main)  # Вывести в десктопе
-#ft.app(target=main, view=ft.WEB_BROWSER)  # Вывести в вебе
+        if record:
+            for key, value in record.items():
+                rowPosition = self.tableWidget.rowCount()
+                self.tableWidget.insertRow(rowPosition)
+                self.tableWidget.setItem(rowPosition, 0, QtWidgets.QTableWidgetItem(str(key)))
+                self.tableWidget.setItem(rowPosition, 1, QtWidgets.QTableWidgetItem(str(value)))
+                self.tableWidget.setItem(rowPosition, 2, QtWidgets.QTableWidgetItem(str(type(value))))
+        self.tableWidget.resizeColumnsToContents()
+
+    def click_combobox(self):
+        print(self.name_comboBox.currentText())
+        self.enter_key_value(ObjectId(self.name_comboBox.currentText()))
+
+
+def main():
+    app = QtWidgets.QApplication(sys.argv)
+    window = mainWin()
+    window.show()
+
+    sys.exit(app.exec_())
+
+
+if __name__ == "__main__":
+    main()
