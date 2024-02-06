1425de664c2474ccf8d57afe1c897f9794022404
Aleksey Tsybikov <tsibikow@mail.ru>
2024-02-06 18:18:27 +0800

    upd 06.02

---
+++ inputWin.py
@@ -0,0 +1,47 @@
+import sys
+from PyQt5.QtWidgets import QApplication, QWidget, QVBoxLayout, QLineEdit, QLabel, QPushButton
+from pymongo import MongoClient
+
+
+class InputWindow(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.initUI()
+        self.client = MongoClient('localhost', 27017)
+        self.db = self.client['test']
+        self.collection = self.db['math']
+
+    def initUI(self):
+        layout = QVBoxLayout()
+
+        self.input_label = QLabel('Введите текст:')
+        layout.addWidget(self.input_label)
+
+        self.input_field = QLineEdit()
+        layout.addWidget(self.input_field)
+
+        self.ok_button = QPushButton('OK')
+        self.ok_button.clicked.connect(self.on_ok_button_click)
+        layout.addWidget(self.ok_button)
+
+        self.setLayout(layout)
+
+        self.setWindowTitle('Окно с полем для ввода и кнопкой')
+        self.show()
+
+    def on_ok_button_click(self):
+        print('clicked')
+        input_text = self.input_field.text()
+        try:
+            # self.collection.create_index(["name"], unique=True)
+            self.collection.insert_one({'name': input_text, 'formula': '', 'variables': '', 'out_value': ''})
+            self.close()
+        except Exception as ex:
+            print("[create_record] Some problem...")
+            print(ex)
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    window = InputWindow()
+    sys.exit(app.exec_())
--- widget.py
+++ widget.py
@@ -3,6 +3,7 @@ from PyQt5 import QtCore, QtGui, QtWidgets
 from bson.objectid import ObjectId
 from pymongo import MongoClient
 import mainWindow
+from inputWin import InputWindow
 
 
 class mainWin(QtWidgets.QMainWindow, mainWindow.Ui_MainWindow):
@@ -15,19 +16,21 @@ class mainWin(QtWidgets.QMainWindow, mainWindow.Ui_MainWindow):
         self.tableWidget.setHorizontalHeaderLabels(['Key', 'Value', 'Type'])
         self.tableWidget.resizeColumnsToContents()
         self.name_comboBox.currentTextChanged.connect(self.click_combobox)
+        self.input_window = None
+        self.addBtn.clicked.connect(self.open_input_window)
         self.load_combobox()
 
     def load_combobox(self):
         all_documents = self.collection.find()
         for document in all_documents:
-            self.name_comboBox.addItem(str(document['_id']))
+            self.name_comboBox.addItem(str(document['name']))
 
-    def enter_key_value(self, id_document):
+    def enter_key_value(self, name_document):
 
         self.tableWidget.clearContents()
         self.tableWidget.setRowCount(0)
 
-        record = self.collection.find_one({"_id": id_document})
+        record = self.collection.find_one({"name": name_document})
 
         if record:
             for key, value in record.items():
@@ -40,7 +43,13 @@ class mainWin(QtWidgets.QMainWindow, mainWindow.Ui_MainWindow):
 
     def click_combobox(self):
         print(self.name_comboBox.currentText())
-        self.enter_key_value(ObjectId(self.name_comboBox.currentText()))
+        self.enter_key_value(str(self.name_comboBox.currentText()))
+
+    def open_input_window(self):
+
+        if self.input_window is None:
+            self.input_window = InputWindow()
+        self.input_window.show()
 
 
 def main():
